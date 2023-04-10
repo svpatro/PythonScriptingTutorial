9af9f3f0266f56a04ca7c7c970229358190ee77d
Shamik Patro <patroshamik@gmail.com>
2023-04-10 11:24:24 -0500

    Updated Files

---
+++ FilterRegex.py
@@ -0,0 +1,17 @@
+# Goal: Fill in the blanks using the provided resources.
+
+import re
+pattern = '' # Fill in this using the regular exression guidebook
+
+f = open('', 'r') # Insert the file you are reading from
+string = f.read()
+found = re.search(pattern, string)
+
+if (found):
+
+    print("expression found!")
+
+    # if the statement is found, write to new file.
+
+else:
+    print("nothing found")
--- script.py
+++
@@ -1,3 +0,0 @@
-# Goal: Fill in the blanks using the provided resources.
-
-import os
